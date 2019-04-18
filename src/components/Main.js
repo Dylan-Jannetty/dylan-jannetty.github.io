f36463c8045acf573586820b69e1a89f2657220e
Dylan <dylan.jannetty@sportshubtech.com>
2019-04-18 16:36:38 -0400

    wtf

--- src/components/Main.js
+++ src/components/Main.js
@@ -58,7 +58,7 @@ class Main extends React.Component {
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
           
-          <form method="POST" name="contact-form" data-netlify="true" Content-Type="application/x-www-form-urlencoded">
+          <form method="POST" action="/success" name="contact-form" data-netlify="true" Content-Type="application/x-www-form-urlencoded">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
