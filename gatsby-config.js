b3b4bbc91746862979d955120a23d54ba04bcd75
Dylan Jannetty <dylanjannetty@dylans-mbp.mynetworksettings.com>
2023-08-28 23:55:21 -0400

    upgrade gatsby and react

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: "Dylan Jannetty Portfolio",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    author: "Dylan Jannetty",
+    description: "Dylan Jannetty's Portfolio"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
